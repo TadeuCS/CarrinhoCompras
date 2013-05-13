1423c48bf669540b68b883f09eb8d570c49e645d
Tadeu <Tadeu@Tadeu-NB.unipam.edu.br>
2013-05-13 19:41:58 -0300

    Terceiro Commit
    configuração template/paginaInicial

---
+++ src/java/Control/ClienteEJB.java
@@ -0,0 +1,33 @@
+package Control;
+
+import Model.Cliente;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+@Stateless
+public class ClienteEJB {
+    
+    @PersistenceContext
+    EntityManager em;
+    
+    public void salvar(Cliente cliente){
+        em.merge(cliente);
+    }
+    
+    public boolean findByLoginSenha(Cliente cliente){
+        Query query = em.createQuery("SELECT u from cliente u WHERE u.nome=:nome AND u.senha=:senha");
+        query.setParameter("nome", cliente.getNome());
+        query.setParameter("senha", cliente.getSenha());
+        System.out.println("testando login para cliente="+cliente.getNome()+" e "+cliente.getEmail()+" e "+cliente.getSenha());
+        if (query.getResultList().size() ==1){
+            System.out.println("Login existe");
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+
+}
